e4b80881b91a8acf828390591b6951cda1f585fb
Noel <noelob@gmail.com>
2025-08-24 15:34:16 -0700

    Handle acknowledgement messages

--- game.go
+++ game.go
@@ -33,11 +33,13 @@ type Game struct {
 	stories      []story
 	deck         deck
 	participants []participant
+
+	ack chan Acknowledgement
 }
 
 func NewGame() *Game {
 	ctx, ctxCancel := context.WithCancel(context.Background())
-	return &Game{ctx: ctx, ctxCancel: ctxCancel, heartbeat: time.NewTicker(45 * time.Second)}
+	return &Game{ctx: ctx, ctxCancel: ctxCancel, heartbeat: time.NewTicker(45 * time.Second), ack: make(chan Acknowledgement, 5)}
 }
 
 func (g *Game) Connect() error {
@@ -75,16 +77,18 @@ func (g *Game) Join(id string) error {
 	g.id = id
 
 	// join game
-	g.send(`{"t":"d","d":{"r":1,"a":"l","b":{"p":"/games/d2538816-2f8e-a8b0-6534-30857b5e932d","h":""}}}`)
-	g.send(`{"t":"d","d":{"r":2,"a":"p","b":{"p":"/games/d2538816-2f8e-a8b0-6534-30857b5e932d/participants/fe24478e-0161-0c97-18ef-ab569207ac44","d":{"fullname":"go-cli","id":"fe24478e-0161-0c97-18ef-ab569207ac44"}}}}`)
-	g.send(`{"t":"d","d":{"r":3,"a":"o","b":{"p":"/games/d2538816-2f8e-a8b0-6534-30857b5e932d/participants/fe24478e-0161-0c97-18ef-ab569207ac44/online","d":{".sv":"timestamp"}}}}`)
-	g.send(`{"t":"d","d":{"r":4,"a":"p","b":{"p":"/games/d2538816-2f8e-a8b0-6534-30857b5e932d/participants/fe24478e-0161-0c97-18ef-ab569207ac44/online","d":true}}}`)
+	g.send(1, `{"t":"d","d":{"r":1,"a":"l","b":{"p":"/games/d2538816-2f8e-a8b0-6534-30857b5e932d","h":""}}}`)
+	g.send(2, `{"t":"d","d":{"r":2,"a":"p","b":{"p":"/games/d2538816-2f8e-a8b0-6534-30857b5e932d/participants/fe24478e-0161-0c97-18ef-ab569207ac44","d":{"fullname":"go-cli","id":"fe24478e-0161-0c97-18ef-ab569207ac44"}}}}`)
+	g.send(3, `{"t":"d","d":{"r":3,"a":"o","b":{"p":"/games/d2538816-2f8e-a8b0-6534-30857b5e932d/participants/fe24478e-0161-0c97-18ef-ab569207ac44/online","d":{".sv":"timestamp"}}}}`)
+	g.send(4, `{"t":"d","d":{"r":4,"a":"p","b":{"p":"/games/d2538816-2f8e-a8b0-6534-30857b5e932d/participants/fe24478e-0161-0c97-18ef-ab569207ac44/online","d":true}}}`)
 
 	return nil
 }
 
 func (g *Game) Leave() error {
 	fmt.Println("Leaving the game")
+
+	close(g.ack)
 
 	// This cancels all derived contexts (i.e. sending and receiving messages)
 	g.ctxCancel()
@@ -97,19 +101,41 @@ func (g *Game) Leave() error {
 	return g.conn.Close(websocket.StatusNormalClosure, "")
 }
 
-func (g *Game) send(payload string) error {
+func (g *Game) send(ref int, payload string) error {
 	ctx, cancel := context.WithTimeout(g.ctx, 5*time.Second)
 	defer cancel()
 
 	println(">>> " + payload)
-	return g.conn.Write(ctx, websocket.MessageText, []byte(payload))
+	err := g.conn.Write(ctx, websocket.MessageText, []byte(payload))
+	if err != nil {
+		return err
+	}
+
+	// Block until an ack message is received
+	if ref > 0 {
+		for {
+			fmt.Printf(">>>> awaiting acknowledgement...\n")
+			ack := <-g.ack
+			if ack.Ref == uint16(ref) {
+				if ack.S == "ok" {
+					fmt.Printf(">>>> message with ref %d acknowledged\n", ref)
+					return nil
+				} else {
+					return Error("unknown acknowledgement status: " + ack.S)
+				}
+			} else {
+				fmt.Printf(">>>> unexpected acknowledgement received; expected %d, discarding: %v\n", ref, ack)
+			}
+		}
+	}
+	return nil
 }
 
 func keepalive(g *Game) {
-	err := g.send("0")
+	err := g.send(0, "0")
 	if err != nil {
 		msg := fmt.Sprintf(">>> Unable to send keepalive: %v", err)
-		fmt.Println(msg)
+		fmt.Printf(msg)
 	}
 }
 
@@ -133,11 +159,13 @@ func listen(g *Game) {
 			msg := fmt.Sprintf("<<< Error parsing message: %v: %s", err, string(bytes))
 			fmt.Println(msg)
 		} else {
-			switch message.(type) {
+			switch msg := message.(type) {
 			case Handshake:
 				fmt.Printf("<<< Received Handshake: %+v\n", message)
 			case Acknowledgement:
 				fmt.Printf("<<< Received Acknowledgement: %+v\n", message)
+				fmt.Printf("<<< Sending on Acknowledgement channel\n")
+				g.ack <- msg
 			case User:
 				fmt.Printf("<<< Received User (has joined the game): %+v\n", message)
 			case Presence:
