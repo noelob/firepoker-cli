f2ad98c97d0090a02df541aaff34aa5a8f78b6e7
Noel <noelob@gmail.com>
2025-09-28 15:35:23 -0700

    Add rudimentary TUI layout

--- ui.go
+++ ui.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/rivo/tview"
+import (
+	"fmt"
+	"github.com/rivo/tview"
+)
 
 func buildUi(game *Game) *tview.Application {
 	app := tview.NewApplication()
 
+	gameData := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetText(fmt.Sprintf("%s\n%s\n", game.state.Name, game.state.Description)).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+
+	gameData.SetBorder(true).SetTitle("Firepoker CLI")
+
 	flex := tview.NewFlex().
 		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-			AddItem(tview.NewBox().SetBorder(true).SetTitle("Firepoker CLI"), 0, 1, false).
+			AddItem(gameData, 5, 1, false).
 			AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
 				AddItem(tview.NewBox().SetBorder(true).SetTitle("Stories"), 0, 2, false).
 				AddItem(tview.NewBox().SetBorder(true).SetTitle("Preview"), 0, 3, false),
